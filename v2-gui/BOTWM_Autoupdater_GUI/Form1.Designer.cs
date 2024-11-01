1bae5153d20f0d319a5909b859e8cad8c071c675
Wesley <wesley@noreply.localhost>
2024-11-01 23:00:35 +0000

    Update Code

--- v2-gui/BOTWM_Autoupdater_GUI/Form1.Designer.cs
+++ v2-gui/BOTWM_Autoupdater_GUI/Form1.Designer.cs
@@ -29,18 +29,18 @@
             listBoxVersions.BorderStyle = BorderStyle.FixedSingle;
             listBoxVersions.FormattingEnabled = true;
             listBoxVersions.ItemHeight = 30;
-            listBoxVersions.Location = new Point(139, 151);
-            listBoxVersions.Margin = new Padding(6, 7, 6, 7);
+            listBoxVersions.Location = new Point(139, 152);
+            listBoxVersions.Margin = new Padding(7, 8, 7, 8);
             listBoxVersions.Name = "listBoxVersions";
             listBoxVersions.Size = new Size(439, 212);
             listBoxVersions.TabIndex = 0;
             // 
             // buttonDownload
             // 
-            buttonDownload.Location = new Point(1, 377);
-            buttonDownload.Margin = new Padding(6, 7, 6, 7);
+            buttonDownload.Location = new Point(2, 376);
+            buttonDownload.Margin = new Padding(7, 8, 7, 8);
             buttonDownload.Name = "buttonDownload";
-            buttonDownload.Size = new Size(720, 53);
+            buttonDownload.Size = new Size(720, 52);
             buttonDownload.TabIndex = 1;
             buttonDownload.Text = "Download";
             buttonDownload.UseVisualStyleBackColor = true;
@@ -51,7 +51,7 @@
             labelStatus.AutoSize = true;
             labelStatus.Dock = DockStyle.Bottom;
             labelStatus.Location = new Point(0, 568);
-            labelStatus.Margin = new Padding(6, 0, 6, 0);
+            labelStatus.Margin = new Padding(7, 0, 7, 0);
             labelStatus.Name = "labelStatus";
             labelStatus.Size = new Size(0, 30);
             labelStatus.TabIndex = 2;
@@ -59,10 +59,11 @@
             // textBox1
             // 
             textBox1.BorderStyle = BorderStyle.None;
-            textBox1.Location = new Point(1, 0);
+            textBox1.Location = new Point(2, 0);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(717, 141);
+            textBox1.Size = new Size(717, 140);
             textBox1.TabIndex = 3;
             textBox1.Text = "\r\nBuilt from the ground up Wesley Hellewell released on Oct 11th 2024\r\nThis auto updater will prepare the mod installation for you\r\nPlease do not close this installer until it is done";
             textBox1.TextAlign = HorizontalAlignment.Center;
@@ -70,14 +71,16 @@
             // progressBarDownload
             // 
             progressBarDownload.Location = new Point(326, 572);
+            progressBarDownload.Margin = new Padding(3, 4, 3, 4);
             progressBarDownload.Name = "progressBarDownload";
-            progressBarDownload.Size = new Size(395, 26);
+            progressBarDownload.Size = new Size(394, 26);
             progressBarDownload.TabIndex = 4;
             // 
             // textBox2
             // 
             textBox2.BorderStyle = BorderStyle.None;
-            textBox2.Location = new Point(150, 2);
+            textBox2.Location = new Point(151, 2);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(528, 28);
             textBox2.TabIndex = 5;
@@ -88,7 +91,7 @@
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(721, 598);
+            ClientSize = new Size(722, 598);
             Controls.Add(textBox2);
             Controls.Add(progressBarDownload);
             Controls.Add(labelStatus);
@@ -96,7 +99,7 @@
             Controls.Add(listBoxVersions);
             Controls.Add(textBox1);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(6, 7, 6, 7);
+            Margin = new Padding(7, 8, 7, 8);
             Name = "Form1";
             Text = "BotW Multiplayer Autoupdater";
             ResumeLayout(false);
